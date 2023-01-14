908429ed122d5bc9a9c9eb2338ff47e85ce9e4ce
unknown <you@example.com>
2023-01-14 20:44:26 +0100

    ejercicios

--- Unidad6/U6T4 - Generador de formularios completo/app.js
+++ Unidad6/U6T4 - Generador de formularios completo/app.js
@@ -14,7 +14,7 @@ inputTexto.addEventListener("click",() =>{
     const input = document.createElement("input")
     input.setAttribute("name",prompt("Dime el nombre que tendrá su atributo name"))
     div.appendChild(input)
-    div.appendChild(br)
+    div.appendChild(creacionbr())
 })
 
 inputPassword.addEventListener("click",() =>{
@@ -22,5 +22,64 @@ inputPassword.addEventListener("click",() =>{
     input.setAttribute("name",prompt("Dime el nombre que tendrá su atributo name"))
     input.setAttribute("type","password")
     div.appendChild(input)
-    div.appendChild(br)
+    div.appendChild(creacionbr())
 })
+
+textArea.addEventListener("click",()=>{
+    const textArea = document.createElement("textarea");
+    textArea.setAttribute("name",prompt("Dime el nombre que tendrá su atributo name"))
+    textArea.setAttribute("column",40);
+    textArea.setAttribute("row",5);
+    div.appendChild(textArea)
+    div.appendChild(creacionbr())
+})
+
+label.addEventListener("click",()=>{
+    const label = document.createElement("label");
+    label.setAttribute("for",prompt("Dime el nombre que tendrá su atributo name"))
+    label.textContent = prompt("Que valor quieres que tenga la etiqueta");
+    div.appendChild(label);
+    div.appendChild(creacionbr());
+})
+
+imagen.addEventListener("click",()=>{
+    const img = document.createElement("img");
+    img.setAttribute("src",prompt("Dime la ruta de la imagen por favor"))
+    div.appendChild(img);
+    div.appendChild(creacionbr());
+})
+
+checkbox.addEventListener("click",()=>{
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    label.textContent = prompt("Que valor quieres que tenga la etiqueta");
+    input.setAttribute("type", "checkbox");
+    div.appendChild(label);
+    div.appendChild(creacionbr());
+    div.appendChild(input);
+    div.appendChild(creacionbr());
+})
+
+radio.addEventListener("click",()=>{
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    label.textContent = prompt("Que valor quieres que tenga la etiqueta");
+    input.setAttribute("type", "radio");
+    div.appendChild(label);
+    div.appendChild(creacionbr());
+    div.appendChild(input);
+    div.appendChild(creacionbr());
+})
+
+boton.addEventListener("click",()=>{
+    const input = document.createElement("input");
+    input.setAttribute("type", "submit");
+    input.setAttribute("value", prompt("Que valor quieres que tenga la etiqueta"));
+    div.appendChild(input);
+    div.appendChild(creacionbr());
+})
+
+function creacionbr() {
+    const br = document.createElement("br");
+    return br
+}
