ed66562e4581768f83d280c0c9bec9e963dae61e
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2023-02-18 05:22:07 +0100

    entregable finalizado

--- Unidad7/U7 - Entregable 21-22/utils.js
+++ Unidad7/U7 - Entregable 21-22/utils.js
@@ -1,6 +1,8 @@
 window.addEventListener('load',()=>{
     const cargarVehiculos = document.getElementById("cargarVehiculos");
+    const enviarvehiculos = document.getElementById("enviarVehiculos");
     cargarVehiculos.addEventListener('click',recogerVehiculos)
+    enviarvehiculos.addEventListener('click',recogerInfoVehiculosRegistro)
 })
 
 function recogerVehiculos() {
@@ -17,19 +19,114 @@ function mostrarVehiculos() {
     console.log('Entro en la funcion mostrarVehiculos');
     if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-            const resultado = JSON.parse(xhr.responseText);
-            crearTabla(resultado); 
+            resultado = JSON.parse(xhr.responseText);
+            crearTabla(resultado);
+            //PHP ya filtra las tablas
+            fetch('insertar_vehiculos.php',{
+                method:'POST',
+                header:{
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(resultado)
+                })
+                    .then((response)=>{
+                        if (response.ok) {
+                            console.log(response)
+                        return response.json();
+                    }
+                    })
+                    .catch((err) => {console.log(err)})
         }
     }
 }
 
+function recogerInfoVehiculosRegistro() {
+    const checkboxAirship = document.getElementById("0").checked;
+    const checkboxAirplane = document.getElementById("1").checked;
+    const checkboxBoat = document.getElementById("2").checked;
+    const divInfo = document.getElementById('breadcrumb');
+
+    const nombre = document.getElementById("nombre").value;
+    const direccion = document.getElementById("direccion").value;
+    const telefono = document.getElementById("telefono").value;
+    const email = document.getElementById("email").value;
+    var id_vehiculo;
+    if (checkboxAirship) {
+        id_vehiculo= document.getElementById("0Vehiculo").textContent;
+    }else if(checkboxAirplane){
+        id_vehiculo= document.getElementById("1Vehiculo").textContent;
+    }else if(checkboxBoat){
+        id_vehiculo= document.getElementById("2Vehiculo").textContent;
+    }
+
+    var vehiculoRegis={
+        "nombre":nombre,
+        "direccion":direccion,
+        "telefono":telefono,
+        "correo":email,
+        "vehiculos":[id_vehiculo]
+    }
+
+
+    fetch('registrar_envio.php',{
+        method: 'POST',
+        header:{
+            'Content-Type': 'application/json'
+        },
+        
+        body: JSON.stringify(vehiculoRegis)
+    })
+        .then((response)=>{
+            if (response.ok) {
+                console.log('Insertado Correctamente')
+                return response.json();
+            }
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+        divInfo.innerHTML ="Dato insertado en la base de datos de registrado correctamente"
+}
+
 function crearTabla(resultado) {
-    crearCabecera(resultado);
-    const tbody = document.createElement('tbody');
-    const tr = document.createElement('tr');
-    tbody.appendChild(tr);
-    for (let i = 0; i < resultado.length; i++) {
-        console.log(resultado[i])
+        crearCabecera(resultado);
+        const tabla = document.getElementById('tabla');
+        const tbody = document.createElement('tbody');
+        const divInfo = document.getElementById('breadcrumb');
+        tabla.appendChild(tbody)
+
+        for (let i = 0; i < resultado.length; i++) {
+            contador = 0;
+            const tr = document.createElement('tr');
+            tbody.appendChild(tr);
+            for (const key in resultado[i]) {
+                const tdID = document.createElement('td');
+                const tdName = document.createElement('td');
+                const tdDescription = document.createElement('td');
+                const tdVehicle = document.createElement('td');
+                const checkbox = document.createElement('input');
+                if (key === "id" || key === "name" || key === "description" || key === "vehicle_class") {
+                    if (contador < 1) {
+                        contador++;
+                        tdID.textContent= resultado[i].id;
+                        tdID.setAttribute("id",i+"Vehiculo");
+                        tdName.textContent = resultado[i].name;
+                        tdDescription.textContent = resultado[i].description;
+                        tdVehicle.textContent = resultado[i].vehicle_class;
+                    
+                        tr.appendChild(tdID);
+                        tr.appendChild(tdName);
+                        tr.appendChild(tdDescription);
+                        tr.appendChild(tdVehicle);
+                        tr.appendChild(checkbox);
+
+                        checkbox.setAttribute("type","checkbox");
+                        checkbox.setAttribute("id",i);
+                }
+                tbody.appendChild(tr);
+            }
+        }
+    divInfo.innerHTML ="Tabla Cargada de vehiculos e Insertada en la base de datos"
     }
 }
 
@@ -50,6 +147,5 @@ function crearCabecera(arrayVehiculos) {
         }
     }
     tr.appendChild(thEnviar);
-
 }
 
